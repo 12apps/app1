c11faeb31965ab02ba94a621a2b921f1bc8c2aec
CodingHouse <sb@codinghouse.co>
2014-12-11 16:51:11 -0800

    testing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,19 @@
 console.log(angular);
 
 var socket = io();
+
+var chatApp = angular.module("chat", []);
+
+chatApp.controller("MainController", function() {
+  var controller = this;
+  controller.messages = [];
+  controller.sendChatMessage = function() {
+    var msg = controller.chatBoxMessage;
+    controller.messages.push(msg);
+    socket.emit('chat message', msg);
+  }
+
+  socket.on('chat message', function(msg){
+    controller.messages.push(msg);
+  });
+});
